4a3b744677d9467fa8228b23d036f7a6d8e31c9c
gjergjk71 <gjergjk71@gmail.com>
2018-08-28 19:24:03 +0200

    Template for register

---
+++ application/auth/register/templates/register.html.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+
+
+</head>
+
+<body>
+
+	<form method="POST">
+		<h4><?php echo $error; ?></h4>
+		Username: <input name="username"><br>
+		Password: <input type="password" name="password"><br>
+		<input type="submit" value="Register">
+	</form>
+
+</body>
+
+</html>
