0a907eed4dfb48e190b3363df810309a464b214c
gjergjk71 <gjergjk71@gmail.com>
2018-08-28 19:23:28 +0200

    Template for login

---
+++ application/auth/login/templates/login.html.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+
+
+</head>
+
+<body>
+
+	<form method="POST">
+		<h4><?php echo $error; ?></h4>
+		<h4><?php echo $message; ?></h4>
+		Username: <input name="username"><br>
+		Password: <input type="password" name="password"><br>
+		<input type="submit" value="Login">
+	</form>
+
+</body>
+
+</html>
