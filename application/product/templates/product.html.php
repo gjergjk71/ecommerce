cf741941cfe918bccd3fe242b3e5421b8bfe1fdc
gjergjk71 <gjergjk71@gmail.com>
2018-08-31 17:47:50 +0200

    Minor change

--- application/product/templates/product.html.php
+++ application/product/templates/product.html.php
@@ -9,7 +9,7 @@
 	Product Description :  <?php echo $product["description"] ?><br>
 	Product Price :  <?php echo $product["price"] ?>$<br>
 	Product Items :  <?php echo count($items) ?><br>
-	<form method="POST" action="<?php echo constant("PROJECT_INDEX") ?>/cart/?add_invoice=<?php echo $product['id'] ?>">
+	<form method="POST" action="<?php echo constant("PROJECT_INDEX") ?>/cart/?add_item=<?php echo $product['id'] ?>">
 		<input type="hidden" name="product_id" value="<?php echo $product['id'] ?>">
 		<input type="submit" value="Add to cart">
 	</form>
